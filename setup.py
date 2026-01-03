66b9ef6b6be30e0a61fc6270c99cb14267115e52
anthonijosap <anthonijosap2022@gmail.com>
2026-01-03 11:05:40 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+hypen = "-e ."
+
+def get_requirements(file_path:str)-> list:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if hypen in requirements:
+            requirements.remove(hypen)
+    return requirements
+
+
+setup(
+    name="mlops",
+    version="0.0.1",
+    author="anthoni",
+    author_email="anthonijosap2022@gmail.com",
+    requires= get_requirements("requirements.txt"),
+    packages=find_packages()
+)

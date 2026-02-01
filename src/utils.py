fc09dd27a3ec962114d7734fdd86a1a383f5fcbc
anthonijosap <anthonijosap2022@gmail.com>
2026-02-01 18:22:36 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
         return model_report
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    import pickle
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
